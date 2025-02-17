516ded31ced7baaed8a8de3549bb005c930660f5
aravind886 <aravindcp@carestack.com>
2025-02-17 22:31:08 +0530

    s

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    PROJECT_URL:process.env.PROJECT_URL
+  }
 };
 
 export default nextConfig;
